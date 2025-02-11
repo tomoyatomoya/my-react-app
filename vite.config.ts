44ce0e479dde950bedc95f71c495a6d053f69bb3
tomoyatomoya <inotomo1098@gmail.com>
2025-02-12 02:06:08 +0900

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// GitHub Pages の URL に合わせて `base` を設定
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/my-word-app/", // ← ここをリポジトリ名に変更！
+});
